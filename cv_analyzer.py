234f60b525a7e79195b370b1f925c8543b79e661
syrineahmed <syrine.ahmed@esprit.tn>
2026-07-01 10:22:38 +0200

    fix - use streamlit secrets for cloud deployment

--- cv_analyzer.py
+++ cv_analyzer.py
@@ -2,9 +2,17 @@ from groq import Groq
 from dotenv import load_dotenv
 import os
 import json
+import streamlit as st
 
-load_dotenv()
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+try:
+    api_key = st.secrets["GROQ_API_KEY"]
+except:
+    from dotenv import load_dotenv
+    load_dotenv()
+    api_key = os.getenv("GROQ_API_KEY")
+
+client = Groq(api_key=api_key)
 
 def translate_result(result, language):
     if language == "English":
